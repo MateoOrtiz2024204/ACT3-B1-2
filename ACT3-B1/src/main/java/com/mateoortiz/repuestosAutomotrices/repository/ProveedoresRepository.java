3d2f52f42b71373c1bd80120382cf3f33c073893
Mateo <mortiz-2024204@kinal.edu.gt>
2026-02-10 20:46:10 -0600

    Feat: Se agrego la logica de ProveedoresRepository

--- ACT3-B1/src/main/java/com/mateoortiz/repuestosAutomotrices/repository/ProveedoresRepository.java
+++ ACT3-B1/src/main/java/com/mateoortiz/repuestosAutomotrices/repository/ProveedoresRepository.java
@@ -1,4 +1,9 @@
 package com.mateoortiz.repuestosAutomotrices.repository;
 
-public interface ProveedoresRepository {
+import com.mateoortiz.repuestosAutomotrices.entity.Proveedor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProveedoresRepository extends JpaRepository<Proveedor,Integer> {
 }
