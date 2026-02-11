12d44f8ffc33c0122c90418f8e526cfa6ee4a507
Mateo <mortiz-2024204@kinal.edu.gt>
2026-02-11 09:55:56 -0600

    Feat: Se agrego la logica de RepuestosServiceImplements

--- ACT3-B1/src/main/java/com/mateoortiz/repuestosAutomotrices/service/RepuestosServiceImplements.java
+++ ACT3-B1/src/main/java/com/mateoortiz/repuestosAutomotrices/service/RepuestosServiceImplements.java
@@ -1,4 +1,51 @@
 package com.mateoortiz.repuestosAutomotrices.service;
 
-public class RepuestosServiceImplements {
+import com.mateoortiz.repuestosAutomotrices.entity.Repuestos;
+import com.mateoortiz.repuestosAutomotrices.repository.RepuestosRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RepuestosServiceImplements implements RepuestosService {
+
+    private final RepuestosRepository repuestosRepository;
+
+    public RepuestosServiceImplements(RepuestosRepository repuestosRepository) {
+        this.repuestosRepository = repuestosRepository;
+    }
+
+    @Override
+    public List<Repuestos> getAllRepuestos() {
+        return repuestosRepository.findAll();
+    }
+
+    @Override
+    public Repuestos getRepuestoById(Integer id) {
+        return repuestosRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Repuestos saveRepuesto(Repuestos repuesto) throws RuntimeException {
+        return repuestosRepository.save(repuesto);
+    }
+
+    @Override
+    public Repuestos updateRepuesto(Integer id, Repuestos repuesto) {
+        Repuestos repuestoExistente = repuestosRepository.findById(id).orElse(null);
+        if (repuestoExistente == null) {
+            return null;
+        }
+        repuestoExistente.setNombreRepuesto(repuesto.getNombreRepuesto());
+        repuestoExistente.setCategoriaRepuesto(repuesto.getCategoriaRepuesto());
+        repuestoExistente.setPrecioCompra(repuesto.getPrecioCompra());
+        repuestoExistente.setPrecioVenta(repuesto.getPrecioVenta());
+        repuestoExistente.setProveedor(repuesto.getProveedor());
+        return repuestosRepository.save(repuestoExistente);
+    }
+
+    @Override
+    public void deleteRepuesto(Integer id) {
+        repuestosRepository.deleteById(id);
+    }
 }
